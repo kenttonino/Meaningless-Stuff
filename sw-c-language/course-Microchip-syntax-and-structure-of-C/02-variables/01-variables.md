86ec0abaa7ba718a3d17b29e7efb345cfd0fc01f
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-11-23 12:22:16 +0800

    Changes.

---
+++ sw-c-language/course-Microchip-syntax-and-structure-of-C/02-variables/01-variables.md
@@ -0,0 +1,7 @@
+## Variable Types
+
+> - Variables consist of type and identifier (name).
+
+> - Part of the ANSI C standard.
+
+> - Implementation can be compiler specific.
---
+++ sw-c-language/course-Microchip-syntax-and-structure-of-C/02-variables/code-variable-types.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void clear_terminal() {
+  system("clear");
+}
+
+void add_newline() {
+  printf("\n");
+}
+
+
+int main() {
+  unsigned int x;
+  unsigned y = 12;
+  int a, b, c;
+  long int myVar = 0x12345678;
+  long z;
+  char first = 'a', second, third = 'c';
+  float big_number = 6.02e+23;
+
+  // Cleanup the screen.
+  clear_terminal();
+  add_newline();
+  add_newline();
+
+  printf("Sizeof Unsigned Integer x: %ld", sizeof(x));
+  add_newline();
+  printf("Sizeof Unsigned Integer y: %ld", sizeof(y));
+  add_newline();
+  printf("Sizeof Integer a: %ld", sizeof(a));
+  add_newline();
+  printf("Sizeof Integer b: %ld", sizeof(b));
+  add_newline();
+  printf("Sizeof Integer c: %ld", sizeof(c));
+  add_newline();
+  printf("Sizeof Long Integer myVar: %ld", sizeof(myVar));
+  add_newline();
+  printf("Sizeof Long z: %ld", sizeof(z));
+  add_newline();
+  printf("Sizeof Character first: %ld", sizeof(first));
+  add_newline();
+  printf("Sizeof Character second: %ld", sizeof(second));
+  add_newline();
+  printf("Sizeof Character third: %ld", sizeof(third));
+  add_newline();
+  printf("Sizeof Float big_number: %ld", sizeof(big_number));
+
+
+  add_newline();
+  add_newline();
+  add_newline();
+  return 0;
+}
