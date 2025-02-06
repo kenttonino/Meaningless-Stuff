ffa5bc4cce309d0622d6bb2bd653c78b89272d69
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2025-02-06 09:28:29 +0800

    Move the data tinkering code.

---
+++ sw-data-structures-and-algorithms/tinkering-cpp/src/examples/examples.hpp
@@ -0,0 +1,7 @@
+#ifndef EXAMPLES_H
+#define EXAMPLES_H
+#include "./01_hello_world.cpp"
+
+void hello_world(void);
+
+#endif
---
+++ sw-data-structures-and-algorithms/tinkering-cpp/src/main.cpp
@@ -0,0 +1,7 @@
+#include "./examples/examples.hpp"
+
+int main(void) {
+  hello_world();
+
+  return 0;
+}
