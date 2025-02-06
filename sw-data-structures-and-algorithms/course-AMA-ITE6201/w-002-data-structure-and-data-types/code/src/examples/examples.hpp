72b93d6ada73a5b8a5559d2a9044f4bbf784375c
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2025-02-06 09:21:49 +0800

    Changes.

---
+++ sw-data-structures-and-algorithms/course-AMA-ITE6201/w-002-data-structure-and-data-types/code/src/examples/examples.hpp
@@ -0,0 +1,7 @@
+#ifndef SAMPLES_H
+#define SAMPLES_H
+#include "./01-hello.cpp"
+
+extern void hello(void);
+
+#endif
---
+++ sw-data-structures-and-algorithms/course-AMA-ITE6201/w-002-data-structure-and-data-types/code/src/main.cpp
@@ -0,0 +1,7 @@
+#include "./examples/examples.hpp"
+
+int main(void) {
+  hello();
+
+  return 0;
+}
